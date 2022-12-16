c9869be5604a05cc13e115025cccd47b1128cd22
Patrick Veverka <veverkap@users.noreply.github.com>
2022-12-15 20:57:08 -0500

    stuff

--- runner.py
+++ runner.py
@@ -1,23 +1,6 @@
 import datetime
 import requests
 
-# load from url
-school_id = "ccff3367-7f5f-4a0d-a8cf-89e1afafe4ba"
-# load tomorrow's date
-serving_date = (datetime.date.today() + datetime.timedelta(days=1)).strftime("%m/%d/%Y")
-# serving_date = datetime.date.today().strftime("%m/%d/%Y")
-url = f"https://webapis.schoolcafe.com/api/CalendarView/GetDailyMenuitems?SchoolId={school_id}&ServingDate={serving_date}&ServingLine=Standard%20Line&MealType=Lunch"
-r = requests.get(url)
-data = r.json()
-print(data)
-message = f"The lunch menu for {serving_date} is:\r\n\r\n"
-
-for item in data["LUNCH"]:
-  message += item["MenuItemDescription"] + "\r\n"
-
-print(message)
-
-
 def send_to_telegram(message):
     apiToken = "1388326080:AAFGxulzcVRIJwSCcQr1pGjddyOwvC5_Fe0"
     chatID = '-1001675706309'
@@ -29,4 +12,21 @@ def send_to_telegram(message):
     except Exception as e:
         print(e)
 
-send_to_telegram(message)
+# load from url
+school_id = "ccff3367-7f5f-4a0d-a8cf-89e1afafe4ba"
+# load tomorrow's date
+serving_date = (datetime.date.today() + datetime.timedelta(days=1)).strftime("%m/%d/%Y")
+# serving_date = datetime.date.today().strftime("%m/%d/%Y")
+url = f"https://webapis.schoolcafe.com/api/CalendarView/GetDailyMenuitems?SchoolId={school_id}&ServingDate={serving_date}&ServingLine=Standard%20Line&MealType=Lunch"
+r = requests.get(url)
+data = r.json()
+print(data)
+
+# message = f"The lunch menu for {serving_date} is:\r\n\r\n"
+
+# for item in data["LUNCH"]:
+#   message += item["MenuItemDescription"] + "\r\n"
+
+# print(message)
+
+# send_to_telegram(message)
