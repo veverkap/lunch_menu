52b666c76f041e84c35988ef6f0954e2f5879429
Patrick Veverka <veverkap@users.noreply.github.com>
2022-12-15 21:00:32 -0500

    ldksafjlk

--- runner.py
+++ runner.py
@@ -1,5 +1,6 @@
 import datetime
 import requests
+import urllib.parse
 
 def send_to_telegram(message):
     apiToken = "1388326080:AAFGxulzcVRIJwSCcQr1pGjddyOwvC5_Fe0"
@@ -17,7 +18,7 @@ school_id = "ccff3367-7f5f-4a0d-a8cf-89e1afafe4ba"
 # load tomorrow's date
 serving_date = (datetime.date.today() + datetime.timedelta(days=1)).strftime("%m/%d/%Y")
 # serving_date = datetime.date.today().strftime("%m/%d/%Y")
-url = f"https://webapis.schoolcafe.com/api/CalendarView/GetDailyMenuitems?SchoolId={school_id}&ServingDate={serving_date}&ServingLine=Standard%20Line&MealType=Lunch"
+url = f"https://webapis.schoolcafe.com/api/CalendarView/GetDailyMenuitems?SchoolId={school_id}&ServingDate={urllib.parse.quote(serving_date)}&ServingLine=Standard%20Line&MealType=Lunch"
 print(url)
 r = requests.get(url)
 data = r.json()
