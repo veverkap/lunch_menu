2f1a39448d81f3951a87cbd6c92e618ce30552b9
Patrick Veverka <veverkap@users.noreply.github.com>
2022-12-15 21:05:12 -0500

    laksdfjslkjl

--- runner.py
+++ runner.py
@@ -16,13 +16,15 @@ def send_to_telegram(message):
 # load from url
 school_id = "ccff3367-7f5f-4a0d-a8cf-89e1afafe4ba"
 # load tomorrow's date
-serving_date = (datetime.date.today() + datetime.timedelta(days=1)).strftime("%m/%d/%Y")
+serving_date = (datetime.date.today() + datetime.timedelta(days=1))
 # serving_date = datetime.date.today().strftime("%m/%d/%Y")
-url = f"https://webapis.schoolcafe.com/api/CalendarView/GetDailyMenuitems?SchoolId={school_id}&ServingDate={urllib.parse.quote(serving_date)}&ServingLine=Standard%20Line&MealType=Lunch"
+serve_date = serving_date.strftime("%m") + "%20" + serving_date.strftime("%d") + "%20" + serving_date.strftime("%Y")
+print(serve_date)
+url = f"https://webapis.schoolcafe.com/api/CalendarView/GetDailyMenuitems?SchoolId={school_id}&ServingDate={serve_date}&ServingLine=Standard%20Line&MealType=Lunch"
 print(url)
-r = requests.get(url)
-data = r.json()
-print(data)
+# r = requests.get(url)
+# data = r.json()
+# print(data)
 
 # message = f"The lunch menu for {serving_date} is:\r\n\r\n"
 
