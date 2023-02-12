3d2f471791d5bf76e8704dfa6a49a08da906b769
Patrick Veverka <veverkap@users.noreply.github.com>
2023-02-12 01:37:46 +0000

    stuff

--- runner.rb
+++ runner.rb
@@ -1,11 +1,12 @@
-require 'net/http'
-require 'uri'
-require 'json'
-require 'date'
-require 'time'
-require 'logger'
-require 'stringio'
 require 'cgi'
+require 'date'
+require 'json'
+require 'logger'
+require 'mailgun-ruby'
+require 'net/http'
+require 'stringio'
+require 'time'
+require 'uri'
 
 LOGGER = Logger.new(STDOUT)
 
@@ -28,11 +29,27 @@ def send_to_telegram(message)
   LOGGER.info "Response: #{response.body}"
 end
 
-school_id = "ccff3367-7f5f-4a0d-a8cf-89e1afafe4ba"
+def send_to_email(email, message)
+  # First, instantiate the Mailgun Client with your API key
+  mg_client = Mailgun::Client.new(ENV['MAILGUN_API_KEY'])
+
+  # Define your message parameters
+  message_params =  {
+    from: 'lunchmenu@mg.veverka.net',
+    to:   email,
+    subject: 'Lunch Menu',
+    text:    message
+  }
+
+  # Send your message through the client
+  mg_client.send_message "mg.veverka.net", message_params
+end
+
 ENV['TZ'] = 'America/New_York'
 date = Date.today
 
 LOGGER.info "Processing date #{date}"
+
 if date.saturday? || date.sunday?
   LOGGER.info "Skipping weekend"
   exit
@@ -55,31 +72,57 @@ message = "Lunch for #{date.strftime("%A, %B %d, %Y")} is: \r\n\r\n"
 
 search_date = date.strftime("%Y-%m-%d")
 
-nl = true
+menu_items = {}
+
+current_section = nil
+has_with = false
+
 values["days"].each do |day|
   if day["date"] == search_date
     day["menu_items"].each do |item|
       if item["is_section_title"]
-        message << "\r\n#{item["text"]}\r\n"
-      end
-      if item.key?("food")
-        food = item["food"]
-        if food.nil?
-          if item.key?("text") && item["text"] == "with"
-            message << " with "
-            nl = false
-          end
-        else
-          if food.key?("name")
-            message << food["name"]
-            message << "\r\n" unless nl
+        current_section = item["text"].gsub("Choose One", "").gsub("-", "").strip
+        menu_items[current_section] = []
+      else
+        next if current_section.nil?
+
+        if item.key?("food")
+          food = item["food"]
+
+          if food.nil?
+            if item.key?("text") && item["text"] == "with"
+              menu_items[current_section][-1] = menu_items[current_section].last + " with"
+              has_with = true
+            end
+          else
+            if food.key?("name")
+              if has_with
+                menu_items[current_section][-1] = menu_items[current_section].last + " " + food["name"]
+                has_with = false
+              else
+                menu_items[current_section] << food["name"]
+              end
+            end
           end
         end
       end
     end
   end
 end
+menu_items.each do |section, items|
+  message << "#{section}: \r\n"
+  items.each do |item|
+    message << "- #{item}\r\n"
+  end
+  message << "\r\n"
+end
 
 LOGGER.info "Sending message: \r\n-----\r\n#{message}"
 
 send_to_telegram(message)
+
+email_addresses = ENV["EMAIL_ADDRESSES"].split(",")
+
+email_addresses.each do |email|
+  send_to_email(email, message)
+end
