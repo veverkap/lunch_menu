2d555d0b92f5fb8538548280c0b66c9b99b861ef
Patrick Veverka <veverkap@users.noreply.github.com>
2025-08-31 18:51:35 -0400

    hoping

--- main.go
+++ main.go
@@ -1,21 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
 	"os"
 )
 
 func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	telegramChesapeakeChatID, ok := os.LookupEnv("TELEGRAM_CHESAPEAKE_CHAT_ID")
 	if !ok {
-		fmt.Println("TELEGRAM_CHESAPEAKE_CHAT_ID not set")
+		logger.Error("TELEGRAM_CHESAPEAKE_CHAT_ID not set")
 		return
 	}
 	skipChesapeake := os.Getenv("SKIP_CHESAPEAKE") == "true"
 
 	if skipChesapeake {
-		fmt.Println("Skipping Chesapeake")
+		logger.Info("Skipping Chesapeake")
 		return
 	}
-	fmt.Printf("Chesapeake chat ID: %s\n", telegramChesapeakeChatID)
+	logger.Info("Chesapeake chat ID: %s", telegramChesapeakeChatID)
 }
