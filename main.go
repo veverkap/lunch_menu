aaf7562c20adf1d33fdee8cbe4fb9dd7c7d6f6c0
Patrick Veverka <veverkap@users.noreply.github.com>
2026-02-19 22:04:46 -0500

    a

--- main.go
+++ main.go
@@ -372,76 +372,70 @@ func sendTelegramMessage(message string) error {
 }
 
 func getTomorrowsWeather(date time.Time) (string, error) {
-	body, err := os.ReadFile("weather.txt")
+	// implementation goes here
+	url := "https://wttr.in/Chesapeake?format=j1"
+	logger.Info("Getting weather", "date", date, "url", url)
+
+	requestStart := time.Now()
+	resp, err := http.Get(url)
 	if err != nil {
-		logger.Error("Failed to read weather response from file", "error", err)
+		logger.Error("Failed to get weather", "error", err)
 		return "", err
 	}
-	return string(body), nil
-	// // implementation goes here
-	// url := "https://wttr.in/Chesapeake?format=j1"
-	// logger.Info("Getting weather", "date", date, "url", url)
+	defer func() {
+		if cerr := resp.Body.Close(); cerr != nil {
+			logger.Error("Failed to close response body", "error", cerr)
+		}
+	}()
+	logger.Info("Weather request completed", "duration", time.Since(requestStart).String())
 
-	// requestStart := time.Now()
-	// resp, err := http.Get(url)
-	// if err != nil {
-	// 	logger.Error("Failed to get weather", "error", err)
-	// 	return "", err
-	// }
-	// defer func() {
-	// 	if cerr := resp.Body.Close(); cerr != nil {
-	// 		logger.Error("Failed to close response body", "error", cerr)
-	// 	}
-	// }()
-	// logger.Info("Weather request completed", "duration", time.Since(requestStart).String())
+	if resp.StatusCode != http.StatusOK {
+		logger.Error("Failed to get weather", "status", resp.Status)
+		return "", fmt.Errorf("failed to get weather: %s", resp.Status)
+	}
 
-	// if resp.StatusCode != http.StatusOK {
-	// 	logger.Error("Failed to get weather", "status", resp.Status)
-	// 	return "", fmt.Errorf("failed to get weather: %s", resp.Status)
-	// }
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		logger.Error("Failed to read weather", "error", err)
+		return "", err
+	}
 
-	// body, err := io.ReadAll(resp.Body)
-	// if err != nil {
-	// 	logger.Error("Failed to read weather", "error", err)
-	// 	return "", err
-	// }
+	// find the forecast for the given date
+	var weather string
+	_, _ = jsonparser.ArrayEach(body, func(value []byte, _ jsonparser.ValueType, _ int, _ error) {
+		dateStr, _ := jsonparser.GetString(value, "date")
+		parsedDate, _ := time.Parse("2006-01-02", dateStr)
+		// compare just the date parts
+		// we assume the date is in local time, so we set the time to noon to avoid any DST issues
+		loc, _ := time.LoadLocation("America/New_York")
+		parsedDate = time.Date(parsedDate.Year(), parsedDate.Month(), parsedDate.Day(), 12, 0, 0, 0, loc)
+		// logger.Info("Parsed date", "dateStr", dateStr, "parsedDate", parsedDate, "targetDate", date)
+		if parsedDate.Equal(date) {
+			// we want to get the weather at 6am, 12pm, and 6pm
+			hourlyWeather := strings.Builder{}
+			_, _ = jsonparser.ArrayEach(value, func(hourlyValue []byte, _ jsonparser.ValueType, _ int, _ error) {
+				timeStr, _ := jsonparser.GetString(hourlyValue, "time")
+				if timeStr == "600" || timeStr == "1200" || timeStr == "1800" {
+					tempF, _ := jsonparser.GetString(hourlyValue, "tempF")
+					weatherDesc, _ := jsonparser.GetString(hourlyValue, "weatherDesc", "[0]", "value")
+					switch timeStr {
+					case "600":
+						timeStr = "6 AM"
+					case "1200":
+						timeStr = "12 PM"
+					case "1800":
+						timeStr = "6 PM"
+					}
+					hourlyWeather.WriteString(fmt.Sprintf("%s - %s°F - %s\n", timeStr, tempF, weatherDesc))
+				}
+			}, "hourly")
+			weather = hourlyWeather.String()
+		}
+	}, "weather")
 
-	// // find the forecast for the given date
-	// var weather string
-	// _, _ = jsonparser.ArrayEach(body, func(value []byte, _ jsonparser.ValueType, _ int, _ error) {
-	// 	dateStr, _ := jsonparser.GetString(value, "date")
-	// 	parsedDate, _ := time.Parse("2006-01-02", dateStr)
-	// 	// compare just the date parts
-	// 	// we assume the date is in local time, so we set the time to noon to avoid any DST issues
-	// 	loc, _ := time.LoadLocation("America/New_York")
-	// 	parsedDate = time.Date(parsedDate.Year(), parsedDate.Month(), parsedDate.Day(), 12, 0, 0, 0, loc)
-	// 	// logger.Info("Parsed date", "dateStr", dateStr, "parsedDate", parsedDate, "targetDate", date)
-	// 	if parsedDate.Equal(date) {
-	// 		// we want to get the weather at 6am, 12pm, and 6pm
-	// 		hourlyWeather := strings.Builder{}
-	// 		_, _ = jsonparser.ArrayEach(value, func(hourlyValue []byte, _ jsonparser.ValueType, _ int, _ error) {
-	// 			timeStr, _ := jsonparser.GetString(hourlyValue, "time")
-	// 			if timeStr == "600" || timeStr == "1200" || timeStr == "1800" {
-	// 				tempF, _ := jsonparser.GetString(hourlyValue, "tempF")
-	// 				weatherDesc, _ := jsonparser.GetString(hourlyValue, "weatherDesc", "[0]", "value")
-	// 				switch timeStr {
-	// 				case "600":
-	// 					timeStr = "6 AM"
-	// 				case "1200":
-	// 					timeStr = "12 PM"
-	// 				case "1800":
-	// 					timeStr = "6 PM"
-	// 				}
-	// 				hourlyWeather.WriteString(fmt.Sprintf("%s - %s°F - %s\n", timeStr, tempF, weatherDesc))
-	// 			}
-	// 		}, "hourly")
-	// 		weather = hourlyWeather.String()
-	// 	}
-	// }, "weather")
-
-	// if weather != "" {
-	// 	return weather, nil
-	// }
-	// logger.Warn("No weather found for date", "date", date)
-	// return "", nil
+	if weather != "" {
+		return weather, nil
+	}
+	logger.Warn("No weather found for date", "date", date)
+	return "", nil
 }
